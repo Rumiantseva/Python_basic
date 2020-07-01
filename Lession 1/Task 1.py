d3baf80dc16a3892aae4a54cbda7f0aa7c3935a5
Rumyantseva Tatyana <TRumyantseva@croc.ru>
2020-07-01 18:34:01 +0300

    Initial commit

---
+++ Lession 1/Task 1.py
@@ -0,0 +1,12 @@
+nameuser = input("Введите Ваше имя: ")
+familynameuser=input("Введите Вашу фамилию: ")
+print("Мое имя: {0} Моя Фамилия: {1} ".format(nameuser,familynameuser))
+
+x = 27
+y = 8
+print("Переменные x и y - ", x, y)
+line1 = input("Введите содержимое первой строки ")
+value1 = int(input("Введите значение 1 "))
+line2 = input("Введите содержимое второй строки ")
+value2 = int(input("Введите значение 2 "))
+print("Введеные значения - %10s %5d %10s %5d" % (line1, value1, line2, value2))
---
+++ Lession 1/Task 3.py
@@ -0,0 +1,3 @@
+n = int(input("Введите любое значение числа n - "))
+total = (n + int(str(n) + str(n)) + int(str(n) + str(n)+ str(n)))
+print("Сумма чисел n + nn + nnn - %d" % total)
